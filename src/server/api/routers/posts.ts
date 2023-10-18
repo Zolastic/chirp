d8dd83c151c68144ab1ebdd4f2a7d049a3c10cf9
Zolastic <honhattienlul@gmail.com>
2023-10-18 23:34:46 +0800

    minor change in posts.ts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -28,21 +28,19 @@ export const postsRouter = createTRPCRouter({
     console.log(users);
 
     return posts.map((post) => {
-      {
-        const author = users.find((user) => user.id === post.authorId);
+      const author = users.find((user) => user.id === post.authorId);
 
-        if (!author || !author.username) {
-          throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
-        }
-
-        return {
-          post: post,
-          author: {
-            ...author,
-            username: author?.username,
-          },
-        };
+      if (!author || !author.username) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
       }
+
+      return {
+        post: post,
+        author: {
+          ...author,
+          username: author?.username,
+        },
+      };
     });
   }),
 });
